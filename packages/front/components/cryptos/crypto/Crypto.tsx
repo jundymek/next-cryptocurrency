7cf6e029d41eb80bd70d94bbef58607b767da634
jundymek <jundymek@gmail.com>
2021-01-04 23:46:50 +0100

    feat: front: Crypto icons

--- packages/front/components/cryptos/crypto/Crypto.tsx
+++ packages/front/components/cryptos/crypto/Crypto.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { CryptoData } from '../utils/useFetchCryptoData';
-
+import { Icon } from 'coinmarketcap-cryptocurrency-icons';
 interface CryptoProps {
   crypto: CryptoData;
 }
 
 const Crypto = React.memo<CryptoProps>(({ crypto }) => {
+  const { firstCurrency, name, price } = crypto;
   return (
     <li className="flex items-center py-4 w-full max-w-md mx-auto bg-blue-100 m-2 p-2 rounded-md">
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center">
+          <div className="mr-2">
+            <Icon i={firstCurrency.toLowerCase()} size={32} />
+          </div>
           <div>
-            <p className="font-mono">{crypto.name}</p>
-            <p className="font-bold text-xs">{crypto.firstCurrency}</p>
+            <p className="font-mono">{name}</p>
+            <p className="font-bold text-xs">{firstCurrency}</p>
           </div>
         </div>
-        <span className="text-xs md:text-xl">{crypto.price} PLN</span>
+        <span className="text-xs md:text-xl">{price} PLN</span>
       </div>
     </li>
   );

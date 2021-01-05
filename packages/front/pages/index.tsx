99409911551abd3f4f203b9d321160a7dee99200
jundymek <jundymek@gmail.com>
2021-01-06 00:15:47 +0100

    feat: front: SettingsIcon

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Cryptos from '../components/cryptos/Cryptos';
+import SettingsIcon from '../components/icons/SettingsIcon';
 
 export default function Home() {
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
@@ -13,8 +14,8 @@ export default function Home() {
         <div className="border-b-2 py-2">
           <h1 className="font-mono text-2xl text-center ">Cryptocurrences</h1>
           <button onClick={handleOptionsOpen}>
-            <div className="w-7 h-7 absolute top-4 right-4 fill-current text-gray-500 hover:text-gray-900">
-              Options
+            <div className="w-7 h-7 absolute top-4 right-4">
+              <SettingsIcon />
             </div>
           </button>
         </div>

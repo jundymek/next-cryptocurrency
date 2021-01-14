8bfe654121679e5becfc497590fb3068832d81f7
jundymek <jundymek@gmail.com>
2021-01-14 23:52:51 +0100

    feat: front: Crypto add animation on price change

--- packages/front/components/cryptos/crypto/Crypto.tsx
+++ packages/front/components/cryptos/crypto/Crypto.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from 'coinmarketcap-cryptocurrency-icons';
 import Arrow from './arrow/Arrow';
 import { CryptoData } from '../Cryptos';
@@ -7,9 +7,20 @@ interface CryptoProps {
 }
 
 const Crypto = React.memo<CryptoProps>(({ crypto }) => {
+  const [animate, setAnimate] = useState('');
   const { firstCurrency, name, price, pair } = crypto;
+
+  useEffect(() => {
+    setAnimate('animate-pulse');
+    setTimeout(() => {
+      setAnimate('');
+    }, 2000);
+  }, [price]);
+
   return (
-    <li className="flex items-center py-4 w-full max-w-md mx-auto bg-blue-100 m-2 p-2 rounded-md">
+    <li
+      className={`flex items-center py-4 w-full max-w-md mx-auto bg-blue-100 m-2 p-2 rounded-md ${animate}`}
+    >
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center">
           <div className="mr-2">

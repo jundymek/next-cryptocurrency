7fc7c7642ef0811f9e5c56fd579bd131c9fe4d98
jundymek <jundymek@gmail.com>
2021-01-17 19:18:01 +0100

    refactor: front: LoginForm component

--- packages/front/pages/login/index.tsx
+++ packages/front/pages/login/index.tsx
@@ -1,115 +1,11 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Layout from '../../components/layout/Layout';
+import LoginForm from '../../components/loginForm/LoginForm';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState<string | undefined>(undefined);
-
-  const router = useRouter();
-
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const payload = { username, password };
-    console.log(username, password);
-    const response = await fetch('http://localhost:3001/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-
-    const { token } = await response.json();
-    if (response.status !== 200) {
-      setError('Credentials not valid');
-      console.log('Credentials not valid');
-    } else {
-      localStorage.setItem('token', token);
-      localStorage.setItem('username', username);
-      router.push('/');
-    }
-  };
-
   return (
     <Layout title="Login page">
-      <div className="flex flex-col content-area  bg-gray-100">
-        <div className="grid place-items-center mx-2 my-20 sm:my-auto">
-          <div
-            className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
-            px-6 py-10 sm:px-10 sm:py-6 
-            bg-white rounded-lg shadow-md lg:shadow-lg"
-          >
-            <h2 className="text-center font-semibold text-3xl lg:text-4xl text-gray-800">Login</h2>
-
-            <form className="mt-10" method="POST" onSubmit={handleSubmit}>
-              <label
-                htmlFor="username"
-                className="block text-xs font-semibold text-gray-600 uppercase"
-              >
-                Username
-              </label>
-              <input
-                id="username"
-                type="username"
-                name="username"
-                placeholder="Username"
-                autoComplete="username"
-                className="block w-full py-3 px-1 mt-2 
-                    text-gray-800 appearance-none 
-                    border-b-2 border-gray-100
-                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                required
-                onChange={handleUsernameChange}
-              />
-
-              <label
-                htmlFor="password"
-                className="block mt-2 text-xs font-semibold text-gray-600 uppercase"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="password"
-                autoComplete="current-password"
-                className="block w-full py-3 px-1 mt-2 mb-4
-                    text-gray-800 appearance-none 
-                    border-b-2 border-gray-100
-                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                required
-                onChange={handlePasswordChange}
-              />
-
-              <button
-                type="submit"
-                className="w-full py-3 mt-10 bg-gray-800 rounded-sm
-                    font-medium text-white uppercase
-                    focus:outline-none hover:bg-gray-700 hover:shadow-none"
-              >
-                Login
-              </button>
-
-              <div className="mt-8 sm:mb-4 text-sm float-right">
-                <a href="register" className="flex-2 underline">
-                  Create an Account
-                </a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      {error && <p>{error}</p>}
+      <LoginForm />
     </Layout>
   );
 };

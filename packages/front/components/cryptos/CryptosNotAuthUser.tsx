51ef37b86c98ca044f07657830ebab83afa667e0
jundymek <jundymek@gmail.com>
2021-02-05 22:49:16 +0100

    feat: Hero component

--- packages/front/components/cryptos/CryptosNotAuthUser.tsx
+++ packages/front/components/cryptos/CryptosNotAuthUser.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from '../hero/Hero';
 import CryptoNotInAsset from './crypto/CryptoNotInAsset';
 import { CryptoData } from './Cryptos';
 
@@ -9,27 +10,7 @@ interface CryptoNotInAssetUserProps {
 const CryptosNotAuthUser = React.memo<CryptoNotInAssetUserProps>(({ visibleCryptos }) => {
   return (
     <>
-      <div className="group self-start float-left transform sm:skew-y-6 text-center sm:text-left w-3/5">
-        <div className="flex flex-col ">
-          <h2 className="font-bold text-gray-700 text-5xl tracking-wide">
-            The world's most popular Bitcoin & cryptocurrency portfolio tracker. Now with trading.
-          </h2>
-          <p className="mt-4 text-gray-500 text-2xl">
-            Track your portfolio, get Blockfolio Signal updates from top teams, and buy and sell
-            crypto with zero fees.
-          </p>
-        </div>
-        {/* <p className="text-lg ml-12 transform duration-200 group-hover:translate-x-10">Simple</p>
-        <p className="text-xl text-right mr-20 transform duration-300 group-hover:translate-x-10">
-          For any currences
-        </p>
-        <p className="text-lg ml-12 transform duration-1000 group-hover:-translate-x-10">
-          Auto refresh
-        </p>
-        <p className="text-xl text-right mr-24 transform duration-500 group-hover:translate-x-4">
-          Monitor your wallet value
-        </p> */}
-      </div>
+      <Hero />
       <ul className="list-none mt-4 p-2 w-full sm:w-1/2 transform sm:skew-y-6 z-10 relative">
         {visibleCryptos?.map((item: CryptoData) => (
           <CryptoNotInAsset key={item.name} crypto={item} />
---
+++ packages/front/components/hero/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="self-start transform sm:skew-y-6 text-center sm:text-left sm:w-3/5 p-4">
+      <div className="flex flex-col ">
+        <h2 className="font-bold text-gray-700 text-2xl sm:text-5xl tracking-wide">
+          The world's most popular Bitcoin & cryptocurrency portfolio tracker. Now with trading.
+        </h2>
+        <p className="mt-4 text-gray-500 text-md sm:text-2xl">
+          Track your portfolio, get Blockfolio Signal updates from top teams, and buy and sell
+          crypto with zero fees.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

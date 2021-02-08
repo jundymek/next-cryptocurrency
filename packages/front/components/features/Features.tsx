7769cd8c3c130b03ff6d8e77a37c20d2c708088a
jundymek <jundymek@gmail.com>
2021-02-08 18:16:51 +0100

    feat: Features buttons

--- packages/front/components/features/Features.tsx
+++ packages/front/components/features/Features.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
-import Feature1 from './Feature1';
-import Feature2 from './Feature2';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
 import Feature from './Feature';
@@ -8,14 +6,47 @@ import Feature from './Feature';
 interface WrapperProps {
   visibleFeature: number;
 }
+interface ButtonProps extends WrapperProps {
+  currentNumber: number;
+}
 
 const Wrapper = styled.div<WrapperProps>`
-  min-width: 200vw;
+  min-width: 100vw;
   overflow: hidden;
   transition-duration: 0.2s;
   transform: translateX(
-    ${(props) => (props.visibleFeature === 1 ? 0 : -props.visibleFeature * 50)}vw
+    ${(props) => (props.visibleFeature === 0 ? 0 : -props.visibleFeature * 100)}vw
   );
+`;
+
+const InnerButton = styled.div<ButtonProps>`
+  transition: min-height 500ms ease;
+  height: 100%;
+  min-height: ${(props) => (props.visibleFeature === props.currentNumber ? '6px' : '1px')};
+  width: 30px;
+  background: ${(props) =>
+    props.visibleFeature === props.currentNumber
+      ? 'linear-gradient(90deg, rgb(106, 17, 203) 0%, rgb(37, 117, 252) 100%)'
+      : 'red'};
+
+  &:hover {
+    width: 30px;
+    max-width: 30px;
+  }
+`;
+
+const StyledButton = styled.button<ButtonProps>`
+  margin: 0 2px;
+  padding: 20px 0;
+  height: 4px;
+  position: relative;
+  &:hover ${InnerButton} {
+    width: 30px;
+    max-width: 30px;
+  }
+  &:focus {
+    /* outline: 0; */
+  }
 `;
 
 const Features = () => {
@@ -41,16 +72,38 @@ const Features = () => {
               text="Automatically receive the latest real-time Blockfolio Signal updates and pricing straight
               to your Slack groups"
             />
+            <Feature
+              title="3 feature"
+              text="Automatically receive the latest real-time Blockfolio Signal updates and pricing straight
+              to your Slack groups"
+            />
           </Wrapper>
         </div>
         <div className="mt-6">
-          <button className="mx-2" onClick={() => handleClick(1)}>
-            First
-          </button>
-          |
-          <button className="mx-2" onClick={() => handleClick(2)}>
-            Second
-          </button>
+          <StyledButton
+            className=""
+            currentNumber={0}
+            visibleFeature={visibleFeature}
+            onClick={() => handleClick(0)}
+          >
+            <InnerButton currentNumber={0} visibleFeature={visibleFeature}></InnerButton>
+          </StyledButton>
+          <StyledButton
+            className=""
+            currentNumber={1}
+            visibleFeature={visibleFeature}
+            onClick={() => handleClick(1)}
+          >
+            <InnerButton currentNumber={1} visibleFeature={visibleFeature}></InnerButton>
+          </StyledButton>
+          <StyledButton
+            className=""
+            currentNumber={2}
+            visibleFeature={visibleFeature}
+            onClick={() => handleClick(2)}
+          >
+            <InnerButton currentNumber={2} visibleFeature={visibleFeature}></InnerButton>
+          </StyledButton>
         </div>
       </div>
     </section>

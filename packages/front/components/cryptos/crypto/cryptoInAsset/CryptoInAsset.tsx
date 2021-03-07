4a914fa25c6060c2ed2ef347fad29992d3443e99
jundymek <jundymek@gmail.com>
2021-03-07 23:56:55 +0100

    style: CryptoInAsset not in box

--- packages/front/components/cryptos/crypto/cryptoInAsset/CryptoInAsset.tsx
+++ packages/front/components/cryptos/crypto/cryptoInAsset/CryptoInAsset.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { CryptoData } from '../../Cryptos';
-import { Icon } from 'coinmarketcap-cryptocurrency-icons';
+import { Icon as CryptoIcon } from 'coinmarketcap-cryptocurrency-icons';
 import styled from 'styled-components';
 // @ts-ignore
 import bgImage from '../../../../assets/bg_circle1.svg';
@@ -12,6 +12,9 @@ import { useAuthState } from '../../../../context/authContext';
 import { Asset, useAssetDispatch } from '../../../../context/assetContext';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import { Icon, InlineIcon } from '@iconify/react';
+import squareEditOutline from '@iconify/icons-mdi/square-edit-outline';
+import deleteOutline from '@iconify/icons-mdi/delete-outline';
 
 interface CryptoProps {
   crypto: CryptoData;
@@ -109,63 +112,86 @@ const CryptoInAsset = React.memo<CryptoProps>(({ crypto, asset }) => {
 
   if (asset?.amount) {
     return (
-      <StyledLi
-        className="group my-10 sm:m-10 w-full sm:w-80 h-80 rounded-md relative flex flex-col items-center border border-gray-600 hover:border-gray-200 z-10 font-mono font-thin"
-        bgImage={bgImage}
-      >
-        {!isEditVisible ? (
-          <>
-            <button
-              className="flex absolute -top-8 left-8  sm:-top-8 sm:-left-8 w-16 h-16 z-20 bg-white rounded-full transform opacity-0 group-hover:opacity-100 duration-1000 group-hover:scale-110  justify-center items-center"
-              onClick={handleToggleEdit}
-            >
-              <EditIcon />
-            </button>
-            <div className="absolute -top-8 left-8  sm:-top-8 sm:-left-8 z-10 transform group-hover:scale-110 duration-1000 group-hover:opacity-0">
-              <Icon i={firstCurrency.toLowerCase()} size={64} />
-            </div>
-          </>
-        ) : (
-          <div className="absolute -top-8 left-8  sm:-top-8 sm:-left-8 z-10 bg-gray-900 w-16 h-16 rounded-full">
-            <div className="relative w-full h-full flex justify-center items-center">
-              <button
-                className="absolute top-2 left-2 mr-4"
-                onClick={handleToggleEdit}
-                title="Cancel"
-              >
-                <CancelIcon />
-              </button>
-              <span className="text-white text-xs">/</span>
-              <button
-                className="absolute bottom-2 right-2 ml-4"
-                onClick={handleOpenDeleteModal}
-                title="Delete"
-              >
-                <DeleteIcon />
-              </button>
-            </div>
+      <li className="w-full h-24 bg-gray-900 flex items-center justify-between my-2 bg-transparent rounded-md px-4">
+        <div className="flex items-center w-4/5 justify-between">
+          <div className="flex items-center">
+            <CryptoIcon i={firstCurrency.toLowerCase()} size={64} />
+            <h3 className="md:text-lg text-white ml-4">
+              {name} <span className="text-md font-bold text-white">({firstCurrency})</span>
+            </h3>
+            <span className="text-white  md:text-4xl md:ml-12 font-mono">{asset.amount}</span>
           </div>
-        )}
 
-        <div className="w-full h-2/5">
-          <div className="flex flex-col justify-center items-center bg-black bg-opacity-25 rounded-t-md h-full relative">
-            <span className="absolute top-0 right-0 p-1 bg-white text-black">{asset.amount}</span>
-            <span className="absolute bottom-0 right-4 text-md pt-10">{price} PLN</span>
-            <h2 className="text-4xl font-bold text-white">{firstCurrency}</h2>
-            <span className="text-lg text-white">{name}</span>
+          <span className="ml-4 md:text-4xl font-mono">
+            {parseFloat((asset?.amount * parseFloat(price)).toPrecision(5))} PLN
+          </span>
+        </div>
+        <div className="flex">
+          <div className="text-gray-300 text-2xl sm:text-4xl md:text-6xl md:mr-4">
+            <Icon icon={squareEditOutline} />
+          </div>
+          <div className="text-gray-300 text-2xl sm:text-4xl md:text-6xl">
+            <Icon icon={deleteOutline} />
           </div>
         </div>
-        <p className="text-4xl text-center my-8 p-2 bg-white text-black transform rotate-1">
-          {parseFloat((asset?.amount * parseFloat(price)).toPrecision(5))}{' '}
-          <span className="text-xs">PLN</span>
-        </p>
-        <CryptoInAssetEditForm
-          isActive={isEditVisible}
-          crypto={crypto}
-          asset={asset}
-          handleOpenDeleteModal={handleOpenDeleteModal}
-        />
-      </StyledLi>
+      </li>
+      // <StyledLi
+      //   className="group my-10 sm:m-10 w-full sm:w-80 h-80 rounded-md relative flex flex-col items-center border border-gray-600 hover:border-gray-200 z-10 font-mono font-thin"
+      //   bgImage={bgImage}
+      // >
+      //   {!isEditVisible ? (
+      //     <>
+      //       <button
+      //         className="flex absolute -top-8 left-8  sm:-top-8 sm:-left-8 w-16 h-16 z-20 bg-white rounded-full transform opacity-0 group-hover:opacity-100 duration-1000 group-hover:scale-110  justify-center items-center"
+      //         onClick={handleToggleEdit}
+      //       >
+      //         <EditIcon />
+      //       </button>
+      //       <div className="absolute -top-8 left-8  sm:-top-8 sm:-left-8 z-10 transform group-hover:scale-110 duration-1000 group-hover:opacity-0">
+      //         <Icon i={firstCurrency.toLowerCase()} size={64} />
+      //       </div>
+      //     </>
+      //   ) : (
+      //     <div className="absolute -top-8 left-8  sm:-top-8 sm:-left-8 z-10 bg-gray-900 w-16 h-16 rounded-full">
+      //       <div className="relative w-full h-full flex justify-center items-center">
+      //         <button
+      //           className="absolute top-2 left-2 mr-4"
+      //           onClick={handleToggleEdit}
+      //           title="Cancel"
+      //         >
+      //           <CancelIcon />
+      //         </button>
+      //         <span className="text-white text-xs">/</span>
+      //         <button
+      //           className="absolute bottom-2 right-2 ml-4"
+      //           onClick={handleOpenDeleteModal}
+      //           title="Delete"
+      //         >
+      //           <DeleteIcon />
+      //         </button>
+      //       </div>
+      //     </div>
+      //   )}
+
+      //   <div className="w-full h-2/5">
+      //     <div className="flex flex-col justify-center items-center bg-black bg-opacity-25 rounded-t-md h-full relative">
+      //       <span className="absolute top-0 right-0 p-1 bg-white text-black">{asset.amount}</span>
+      //       <span className="absolute bottom-0 right-4 text-md pt-10">{price} PLN</span>
+      //       <h2 className="text-4xl font-bold text-white">{firstCurrency}</h2>
+      //       <span className="text-lg text-white">{name}</span>
+      //     </div>
+      //   </div>
+      //   <p className="text-4xl text-center my-8 p-2 bg-white text-black transform rotate-1">
+      //     {parseFloat((asset?.amount * parseFloat(price)).toPrecision(5))}{' '}
+      //     <span className="text-xs">PLN</span>
+      //   </p>
+      //   <CryptoInAssetEditForm
+      //     isActive={isEditVisible}
+      //     crypto={crypto}
+      //     asset={asset}
+      //     handleOpenDeleteModal={handleOpenDeleteModal}
+      //   />
+      // </StyledLi>
     );
   }
   return null;
--- packages/front/components/cryptos/cryptosAuthUser/CryptosAuthUser.tsx
+++ packages/front/components/cryptos/cryptosAuthUser/CryptosAuthUser.tsx
@@ -34,26 +34,29 @@ const CryptosAuthUser = React.memo<CryptoInAssetUserProps>(({ visibleCryptos }) 
   return (
     <div className="w-full flex flex-col">
       <div className="w-full p-2 text-white z-10 flex flex-col justify-center items-center">
-        <ul className="list-none mt-4 p-2 w-full ms:w-2/3 mx-auto flex flex-wrap justify-center">
+        <h2 className="self-start text-4xl">Your assets</h2>
+        <ul className="list-none mt-4 py-2 w-full ms:w-2/3 mx-auto flex flex-wrap justify-center">
           {visibleCryptos?.map((item: CryptoData) => (
             <CryptoInAsset key={item.name} crypto={item} asset={getAsset(item.firstCurrency)} />
           ))}
         </ul>
         {notInAssets.length > 0 && !isAddFormVisible && (
-          <ActionButton handleFunction={toggleAddFormVisible} text="add new asset" />
+          <div className="mt-8">
+            <ActionButton handleFunction={toggleAddFormVisible} text="add new asset" />
+          </div>
         )}
         {isAddFormVisible && (
           <AddNewAssetForm cryptos={notInAssets} toggleAddFormVisible={toggleAddFormVisible} />
         )}
       </div>
-      <div className="w-full">
+      {/* <div className="w-full">
         Not in portfolio:
         <ul className="list-none mt-4 p-2 w-full sm:w-2/3 mx-auto transform grid grid-cols-1 lg:grid-cols-2 gap-2 col-span-4">
           {notInAssets.map((item: CryptoData) => (
             <CryptoNotInAsset key={item.name} crypto={item} />
           ))}
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 });
